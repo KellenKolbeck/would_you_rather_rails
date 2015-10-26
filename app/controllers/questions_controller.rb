ab4b257591aa519cf5baa7c53425b01df909bbf5
Kellen Kolbeck <kellengkolbeck@gmail.com>
2015-10-26 16:42:31 -0700

    Seed database. Add full CRUD functionality for questions

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,4 +2,34 @@ class QuestionsController < ApplicationController
   def index
     @questions = Question.all
   end
+
+  def new
+    @question = Question.new
+  end
+
+  def create
+    @question = Question.new
+    if @question.save()
+      redirect_to questions_path
+    else
+      render 'new'
+  end
+
+  def edit
+    @question = Question.find(params[:id])
+  end
+
+  def update
+    @question = Question.find(params[:id])
+    if @question.update
+      redirect_to questions_path
+    else
+      render 'edit'
+  end
+
+  def destroy
+    @question = Question.find(params[:id])
+    @question.destroy
+    redirect_to questions_path
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+Question.create(title: "Would you rather spend the day...", optionA: "on a mountain?", optionB: "under the ocean?")
+Question.create(title: "Would you rather own...", optionA: "a private island?", optionB: "a jet to fly you anywhere?")
+Question.create(title: "Would you rather meet...", optionA: "Jimi Hendrix?", optionB: "Jerry Garcia?")
+Question.create(title: "Would you rather eat...", optionA: "cockroaches?", optionB: "slugs?")
+Question.create(title: "Would you rather...", optionA: "have an arrainged marriage?", optionB: "before forever single?")
